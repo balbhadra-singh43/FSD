1e469a672e1b206b0df8170968bcef05566cbd03
Balbhadra Singh <balbhadra.25b01010361@abes.ac.in>
2026-07-23 16:24:58 +0530

    done task 1

--- lab 1/eventloop/task1.js
+++ lab 1/eventloop/task1.js
@@ -1,18 +1,35 @@
 const f1 = () => {
-    console.log("f1");
+  console.log("f1 starts");
+  f2();
+  console.log("f1 running");
+  console.log("f1 end");
 };
 const f2 = () => {
-    console.log("f2");
+  console.log("f2 starts");
+  f3();
+  console.log("f2 running");
+  console.log("f2 ends");
 };
 const f3 = () => {
-    console.log("f3");
+  console.log("f3 starts");
+  console.log("f3 running");
+  console.log("f3 end");
 };
-function main () {
-    console.log("main");
-    f1();
-    f2();
-    f3();
-    console.log("end main");
-
+function main() {
+  console.log("main");
+  f1();
+  console.log("end main");
 }
 main();
+
+
+// javascript is synchronous and single-threaded, which means it executes code line by line in the order it is written.
+
+// In asynchronous we use event loop to manage thhe call stack
+
+// Asynchronous calls using timers
+// 1 SetTimeout
+// 2 Setimmediate
+// 3 Process.nextTick
+// 4 Set interval
+
